f8a652af7c8778879bceda0104388e2ce07824d1
hypatia-of-sva <121448956+hypatia-of-sva@users.noreply.github.com>
2025-02-08 20:49:14 +0100

    Add files via upload

---
+++ sess-2/util.c
@@ -0,0 +1,39 @@
+#include "common.h"
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+float clamp(float val, float min, float max) {
+    if(val < min) return min;
+    if(val > max) return max;
+    return val;
+}
+
+size_t filesize(FILE* fp) {
+    struct stat buf;
+    int fd = fileno(fp);
+    fstat(fd, &buf);
+    off_t size = buf.st_size;
+    return size;
+}
+
+void die(char* str) {
+	fprintf(stderr, str);
+	exit(EXIT_FAILURE);
+}
+
+bool is_power_of_2(uint32_t x) {
+    return x > 0 && !(x & (x-1));
+}
+
+uint32_t truncate_power_of_2(uint32_t x) {
+    for(int bit = 31; bit > 0; bit--) {
+        uint32_t pow = (1 << bit);
+        if(x>=pow) return pow;
+    }
+    return 0;
+}
