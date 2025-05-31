acc35a620d642b20d25b5fd5098cacf2f6f4cd48
hypatia-of-sva <121448956+hypatia-of-sva@users.noreply.github.com>
2025-05-31 20:49:12 +0200

    Add files via upload

---
+++ sess-17/build.bat
@@ -0,0 +1,6 @@
+PATH=C:\raylib\w64devkit\bin;%PATH%
+del .\tool.exe
+gcc -gdwarf -o tool.exe *.c  C:\raylib\raylib\src\raylib.rc.data -static -O2 -std=c99 -Wall -IC:\raylib\raylib\src -Iexternal -DPLATFORM_DESKTOP -lraylib -lopengl32 -lgdi32 -lwinmm -lm
+.\tool.exe
+pause
+
